d95b2608f2a2adf85d7825b0723a30b417df4b52
Tobias <tobias.fetzer97@googlemail.com>
2018-05-01 13:36:11 +0200

    Added ActionButtonPosition to git

---
+++ src/ActualButtonPosition.java
@@ -0,0 +1,11 @@
+/**
+ * The position of the non rotated Button Button
+ */
+public class ActualButtonPosition {
+    public int x;
+    public int y;
+    public ActualButtonPosition(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
